e90e7da7ed806b4a14e45acefc30173aa52ce1aa
Jordan Marine <marine.jordan64@gmail.com>
2025-01-23 19:03:10 -0600

    Moved whitelist json.

--- lua/autorun/server/sv_whitelist.lua
+++ lua/autorun/server/sv_whitelist.lua
@@ -1,4 +1,4 @@
 hook.Add("CheckPassword", "ServerWhitelist", function(steamID64)
-	local whitelistTable = util.JSONToTable(file.Read("whitelist.json", true) or "{}")
+	local whitelistTable = util.JSONToTable(file.Read("whitelist.json") or "{}")
 	if not whitelistTable[steamID64] then return false, "You must be whitelisted to join this server." end
 end)
