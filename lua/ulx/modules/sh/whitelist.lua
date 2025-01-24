2627532b9b557be4317f5bc768a9df2cffeae8da
Jordan Marine <marine.jordan64@gmail.com>
2025-01-23 19:28:30 -0600

    Fixed response text missing a space.

--- lua/ulx/modules/sh/whitelist.lua
+++ lua/ulx/modules/sh/whitelist.lua
@@ -3,7 +3,7 @@ local function addToWhitelist(calling_ply, steamID64)
 	local whitelistTable = util.JSONToTable(file.Read("whitelist.json") or "{}", false, true)
 	whitelistTable[steamID64] = true
 	file.Write("whitelist.json", util.TableToJSON(whitelistTable, true))
-	ULib.tsay(calling_ply, "Updated the whitelist to include" .. steamID64 .. ".")
+	ULib.tsay(calling_ply, "Updated the whitelist to include " .. steamID64 .. ".")
 end
 
 local whitelistcmd = ulx.command(CATEGORY_NAME, "ulx whitelist", addToWhitelist, "!whitelist")
