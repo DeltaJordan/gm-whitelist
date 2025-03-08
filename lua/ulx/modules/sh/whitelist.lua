448f78768805a82d9eb3311ed4be1788022431bd
Jordan Marine <marine.jordan64@gmail.com>
2025-03-08 15:48:51 -0600

    Added steam id input validation.

--- lua/ulx/modules/sh/whitelist.lua
+++ lua/ulx/modules/sh/whitelist.lua
@@ -1,5 +1,10 @@
 local CATEGORY_NAME = "Utility"
 local function addToWhitelist(calling_ply, steamID64)
+	if util.SteamIDFrom64(steamID64) == "STEAM_0:0:0" then
+		ULib.tsay(calling_ply, "Invalid SteamID64. Please check input.")
+		return
+	end
+
 	local whitelistTable = util.JSONToTable(file.Read("whitelist.json") or "{}", false, true)
 	whitelistTable[steamID64] = true
 	file.Write("whitelist.json", util.TableToJSON(whitelistTable, true))
